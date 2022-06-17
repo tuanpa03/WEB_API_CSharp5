1c7fb04ff8e13c59ac7a407d50a02c9debb45374
tuanpa03 <tuanpaph18296@fpt.edu.vn>
2022-06-17 18:22:51 +0700

    CRUD MVC table_Roles

---
+++ Web_BanHang/Controllers/RolesController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Website_BanHang.Models;
+
+namespace Web_BanHang.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly BanHangContext _context;
+
+        public RolesController(BanHangContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Roles
+        public async Task<IActionResult> Index()
+        {
+              return _context.Roles != null ? 
+                          View(await _context.Roles.ToListAsync()) :
+                          Problem("Entity set 'BanHangContext.Roles'  is null.");
+        }
+
+        // GET: Roles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Roles == null)
+            {
+                return NotFound();
+            }
+
+            var roles = await _context.Roles
+                .FirstOrDefaultAsync(m => m.RoleCode == id);
+            if (roles == null)
+            {
+                return NotFound();
+            }
+
+            return View(roles);
+        }
+
+        // GET: Roles/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Roles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RoleCode,RoleName,Description")] Roles roles)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(roles);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(roles);
+        }
+
+        // GET: Roles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Roles == null)
+            {
+                return NotFound();
+            }
+
+            var roles = await _context.Roles.FindAsync(id);
+            if (roles == null)
+            {
+                return NotFound();
+            }
+            return View(roles);
+        }
+
+        // POST: Roles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("RoleCode,RoleName,Description")] Roles roles)
+        {
+            if (id != roles.RoleCode)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(roles);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RolesExists(roles.RoleCode))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(roles);
+        }
+
+        // GET: Roles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Roles == null)
+            {
+                return NotFound();
+            }
+
+            var roles = await _context.Roles
+                .FirstOrDefaultAsync(m => m.RoleCode == id);
+            if (roles == null)
+            {
+                return NotFound();
+            }
+
+            return View(roles);
+        }
+
+        // POST: Roles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Roles == null)
+            {
+                return Problem("Entity set 'BanHangContext.Roles'  is null.");
+            }
+            var roles = await _context.Roles.FindAsync(id);
+            if (roles != null)
+            {
+                _context.Roles.Remove(roles);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RolesExists(int id)
+        {
+          return (_context.Roles?.Any(e => e.RoleCode == id)).GetValueOrDefault();
+        }
+    }
+}
--- Web_BanHang/Models/Roles.cs
+++ Web_BanHang/Models/Roles.cs
@@ -3,6 +3,10 @@ namespace Website_BanHang.Models
 {
     public class Roles
     {
+        public Roles()
+        {
+            staffs = new List<Staffs>();
+        }
         public int RoleCode { get; set; }
         public string RoleName { get; set; }
         public string Description { get; set; }
