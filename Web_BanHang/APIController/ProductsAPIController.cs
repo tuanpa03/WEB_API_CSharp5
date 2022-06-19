b8128b68ca3c64e344b9fbb57b20a257d3689be7
hieuptph18134 <93701031+hieuptph18134@users.noreply.github.com>
2022-06-19 22:20:17 +0700

    up crud api products và show sản phẩm

---
+++ Web_BanHang/APIController/ProductsAPIController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Website_BanHang.Models;
+
+namespace Web_BanHang.APIController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsAPIController : ControllerBase
+    {
+        private readonly BanHangContext _context;
+
+        public ProductsAPIController(BanHangContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("api-get-list-sp")]
+        public List<Products> GetListProducts()
+        {
+            List<Products> result = _context.Products.Where(p => p.Quantity > 0).ToList();
+            return result;
+        }
+        // GET: api/ProductsAPI
+        [HttpGet]
+        [Route("get-products")]
+        public async Task<ActionResult<IEnumerable<Products>>> GetProducts()
+        {
+          if (_context.Products == null)
+          {
+              return NotFound();
+          }
+            return await _context.Products.ToListAsync();
+        }
+
+        // GET: api/ProductsAPI/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Products>> GetProducts(int id)
+        {
+          if (_context.Products == null)
+          {
+              return NotFound();
+          }
+            var products = await _context.Products.FindAsync(id);
+
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return products;
+        }
+
+        // PUT: api/ProductsAPI/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProducts(int id, Products products)
+        {
+            if (id != products.ProductCode)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(products).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProductsAPI
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [Route("add-products")]
+        public async Task<ActionResult<Products>> PostProducts(Products products)
+        {
+          if (_context.Products == null)
+          {
+              return Problem("Entity set 'BanHangContext.Products'  is null.");
+          }
+            _context.Products.Add(products);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProducts", new { id = products.ProductCode }, products);
+        }
+
+        // DELETE: api/ProductsAPI/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProducts(int id)
+        {
+            if (_context.Products == null)
+            {
+                return NotFound();
+            }
+            var products = await _context.Products.FindAsync(id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(products);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductsExists(int id)
+        {
+            return (_context.Products?.Any(e => e.ProductCode == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Web_BanHang/Controllers/ProductsController.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using Website_BanHang.Models;
+
+namespace Web_BanHang.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly BanHangContext _context;
+        private readonly HttpClient _client = new HttpClient();
+
+        public ProductsController(BanHangContext context)
+        {
+            _context = context;
+            _client.BaseAddress = new Uri("https://localhost:7138/");
+        }
+        public IActionResult GetProducts()//show sản phẩm ra trang chủ
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7138/");
+            var jsonConnect = client.GetAsync("api/ProductsAPI/get-products").Result;//kh được thì thử đuôi này xem api-get-list-sp
+            string jsonData = jsonConnect.Content.ReadAsStringAsync().Result;
+            //ViewData["data"] = jsonData; 
+            //lấy list người từ api 
+            var model = JsonConvert.DeserializeObject<List<Products>>(jsonData);
+            return View(model);
+        }
+        public async Task<IActionResult> Index()
+        {
+            var jsonContent = await _client.GetAsync("api/ProductsAPI/get-products");
+            var jsonData = await jsonContent.Content.ReadAsStringAsync();
+            var model = JsonConvert.DeserializeObject<List<Products>>(jsonData);
+            return View(model);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var fragrant = await _context.Products
+                .FirstOrDefaultAsync(m => m.ProductCode == id);
+            if (fragrant == null)
+            {
+                return NotFound();
+            }
+
+            return View(fragrant);
+        }
+
+        // GET: Admin/ProductType/Create
+
+
+        public IActionResult Create()
+        {
+            ViewData["CatCode"] = new SelectList(_context.Categroies, "CatCode", "CatName");
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductCode,CatCode,ProductName,Quantity,Image,Price,Note")] Products products)
+        {
+            HttpClient client = new HttpClient();//set đường dẫn cơ bản 
+            client.BaseAddress = new Uri("https://localhost:7138/add-products");
+            if (true)
+            {
+                _context.Add(products);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CatCode"] = new SelectList(_context.Categroies, "CatCode", "CatName", products.CatCode);
+            return View(products);
+        }
+        //[HttpPost]
+        //public async Task<IActionResult> Create(Products fragrant)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var myContent = JsonConvert.SerializeObject(fragrant);
+        //        var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+        //        var byContent = new ByteArrayContent(buffer);
+        //        byContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+        //        await _client.PostAsync("api/ProductsAPI/add-products", byContent);
+
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View();
+        //}
+        // GET: Admin/ProductType/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var fragrant = await _context.Products.FindAsync(id);
+            if (fragrant == null)
+            {
+                return NotFound();
+            }
+            return View(fragrant);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Products products)
+        {
+            if (id != products.ProductCode)
+            {
+                return NotFound();
+            }
+
+            if (true)
+            {
+                try
+                {
+                    await _client.PutAsJsonAsync<Products>($"api/ProductsAPI/{id}", products);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FragrantExists(products.ProductCode))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(products);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.Products
+                .FirstOrDefaultAsync(m => m.ProductCode == id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return View(products);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Products == null)
+            {
+                return Problem("Entity set 'BanHangContext.Products'  is null.");
+            }
+
+            await _client.DeleteAsync($"api/ProductsAPI/{id}");
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        private bool FragrantExists(int id)
+        {
+            return (_context.Products?.Any(e => e.ProductCode == id)).GetValueOrDefault();
+        }
+    }
+}
